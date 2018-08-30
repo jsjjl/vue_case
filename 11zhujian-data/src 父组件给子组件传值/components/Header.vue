8f9f341b88effe1bbc7939df9b9ad35f6b6bb9f8
jsjjl <135652820@qq.com>
2018-08-30 17:58:33 +0800

    all

---
+++ 11zhujian-data/src 父组件给子组件传值/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+
+
+    <div>
+    
+        <h2>我是头部组件--{{a}}</h2>
+
+      
+
+       
+    </div>
+</template>
+
+
+
+
+<script>
+    
+export default{
+
+
+    data(){
+
+        return{
+            msg:'子组件的msg'
+        }
+    },
+    methods:{
+       
+
+    },
+
+    //https://cn.vuejs.org/v2/guide/components.html#Prop-验证
+    props:{
+
+        'a':String      
+    }
+    
+}
+
+</script>
